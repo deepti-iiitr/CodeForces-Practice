0b4474d45b7c401ecdfe8d0def65ae5a90048e24
deepti23-web <64003317+deepti23-web@users.noreply.github.com>
2021-11-12 15:47:09 +0530

    updating

---
+++ A_Linear_Keyboard.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define endl ("\n")
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+
+//Deepti Template 
+
+#define for(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+
+void solve(){
+        string alpha;
+        cin>>alpha;
+        string s;
+        cin>>s;
+        if(s.length()==1)
+          cout<<"0"<<endl;
+        else{
+            int sum=0;
+            int i=0;
+        for(i,s.length()-1){
+            int  found1= alpha.find(s[i]);
+            int found2= alpha.find(s[i+1]);
+            sum= sum+ abs(found1- found2);
+        }
+        cout<<sum<<endl;
+        }
+        
+}
+int main()
+{
+	fast_cin();
+	ll t;
+	cin >> t;
+	while(t--) {
+	
+		solve();
+	}
+	return 0;
+}
