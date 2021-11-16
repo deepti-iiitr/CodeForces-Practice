39a58324ab26b0d49314368dc2aba56faf9b48dc
deepti23-web <64003317+deepti23-web@users.noreply.github.com>
2021-11-16 12:55:30 +0530

    more codes

---
+++ A_Shortest_Path_with_Obstacle.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define endl ("\n")
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 10e+7;
+
+//Deepti Template 
+
+
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+
+void solve(){
+   vector<int> a(2), b(2), f(2);
+cin >> a[0] >> a[1];
+cin >> b[0] >> b[1];
+cin >> f[0] >> f[1];
+int ans = abs(a[0] - b[0]) + abs(a[1] - b[1]);
+if ((a[0] == b[0] && a[0] == f[0] && min(a[1], b[1]) < f[1] && f[1] < max(a[1], b[1]))
+        || (a[1] == b[1] && a[1] == f[1] && min(a[0], b[0]) < f[0] && f[0] < max(a[0], b[0])))
+    ans += 2;
+cout << ans << endl;
+}
+int main()
+{
+	fast_cin();
+	ll t;
+	cin >> t;
+	while(t--) {
+	
+		solve();
+	}
+	return 0;
+}
