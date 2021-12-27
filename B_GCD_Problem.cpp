db33b19854cbc70dbafcdaa386601f83fc03b04d
deepti23-web <64003317+deepti23-web@users.noreply.github.com>
2021-12-27 17:04:31 +0530

    more codes

---
+++ B_GCD_Problem.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define endl ("\n")
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 10e+7;
+
+//Deepti Template 
+
+
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+int gcd(int a, int b)
+{
+    // Everything divides 0
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+  
+    // base case
+    if (a == b)
+        return a;
+  
+    // a is greater
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    //cout<<n;
+    ll a=2,b=n-1-a;
+    //n=n-1;
+    while(true){
+        int x= gcd(a,b);
+       
+        if(x==1)
+       { cout<<a<<" "<<b<<" "<<"1"<<endl;
+          break;
+          }
+           a++;
+        b=n-a-1;
+    }
+ 
+}
+int main()
+{
+	fast_cin();
+	ll t;
+	cin >> t;
+	while(t--) {
+	     // cout<<"solve";
+		solve();
+	}
+	return 0;
+}
