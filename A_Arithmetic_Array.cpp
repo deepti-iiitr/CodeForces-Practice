650057113a5bd4e9842a365e911c21a93bf7f67b
deepti23-web <64003317+deepti23-web@users.noreply.github.com>
2022-01-06 12:42:18 +0530

    more codes

---
+++ A_Arithmetic_Array.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define endl ("\n")
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 10e+7;
+
+//Deepti Template 
+
+
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll arr[n];
+    for(int i=0;i<n;i++)
+       cin>>arr[i];
+    ll sum=0;
+    for(int i=0;i<n;i++){
+        sum+= arr[i];
+    }
+    if(sum < n)
+      cout<<"1"<<endl;
+    else
+      cout<<sum-n<<endl;
+}
+int main()
+{
+	fast_cin();
+	ll t;
+	cin >> t;
+	while(t--) {
+	
+		solve();
+	}
+	return 0;
+}
