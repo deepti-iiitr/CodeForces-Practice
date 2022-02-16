5f1b92066d1a1fc48190cecb98f2eb589fe6dc37
deepti23-web <64003317+deepti23-web@users.noreply.github.com>
2022-02-16 18:58:07 +0530

    more codes

---
+++ B_Odd_Swap_Sort.cpp
@@ -0,0 +1,71 @@
+
+#include <bits/stdc++.h>
+#define endl ("\n")
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 10e+7;
+
+//Deepti Template 
+
+
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll a[n];
+    vector<int>even,odd,v1,v2;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        if(a[i] % 2==0)
+          {
+              even.push_back(a[i]);
+              v1.push_back(a[i]);
+          }
+        else
+         {
+              odd.push_back(a[i]);
+              v2.push_back(a[i]);
+         }
+    }
+    sort(v1.begin(),v1.end());
+    sort(v2.begin(),v2.end());
+    if(v1==even and v2==odd)
+       cout<<"YES"<<endl;
+    else
+       cout<<"NO"<<endl;
+}
+int main()
+{
+	fast_cin();
+	ll t;
+	cin >> t;
+	while(t--) {
+	
+		solve();
+	}
+	return 0;
+}
